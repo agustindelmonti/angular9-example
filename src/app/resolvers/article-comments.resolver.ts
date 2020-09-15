99b6c5f4fd217e547288a42906ae07274aac51bc
lucasrandisi <lucasrandisi@gmail.com>
2020-09-15 20:09:44 -0300

    Resolvers

---
+++ src/app/resolvers/article-comments.resolver.ts
@@ -0,0 +1,19 @@
+import { Resolve, ActivatedRoute, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Injectable } from '@angular/core';
+
+import { ArticleService } from '../services/article.service';
+
+
+@Injectable({ providedIn: 'root' })
+export class ArticleCommentsResolver implements Resolve<any> {
+    constructor(private service: ArticleService) {}
+
+    resolve(
+        route: ActivatedRouteSnapshot,
+        state: RouterStateSnapshot,
+    ): any {
+        const slug: string = route.params['articleSlug'];
+
+        return this.service.getArticleComments(slug);
+    }
+}
